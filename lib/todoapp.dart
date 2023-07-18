0a4a4af59a01bf6491dbc207af7ba6261c3dd15e
simar295 <simarconnect@gmail.com>
2023-07-19 02:56:33 +0530

    more firebase functionalities

--- lib/todoapp.dart
+++ lib/todoapp.dart
@@ -29,7 +29,8 @@ class _todoappState extends State<todoapp> {
     final geturl = Uri.https(
         'todo-app-f7776-default-rtdb.firebaseio.com', "todo-data.json");
     final getresponse = await http.get(geturl);
-    print(getresponse.body);
+
+    print(getresponse.statusCode);
 
     final Map listdata = json.decode(getresponse.body);
     for (final item in listdata.entries) {
@@ -39,9 +40,10 @@ class _todoappState extends State<todoapp> {
     }
   }
 
-  final List mytodo = [
-    /* ["mywork", false],
-    ["exercise", true], */
+  List newtodo = [];
+  List mytodo = [
+    /*  ["mywork", false],
+    ["exercise", true],  */
   ];
 
   void submitdata() async {
@@ -56,9 +58,6 @@ class _todoappState extends State<todoapp> {
         ),
       );
     } else {
-      setState(() {
-        mytodo.add([givecontroller.text, false]);
-      });
       //firebase sending data
       final url = Uri.https(
           'todo-app-f7776-default-rtdb.firebaseio.com', "todo-data.json");
@@ -71,20 +70,33 @@ class _todoappState extends State<todoapp> {
           "data": givecontroller.text,
         }),
       );
-      print(response.body);
-      print(response.statusCode);
+
       if (!context.mounted) {
         return;
       }
       Navigator.of(context).pop();
+      setState(() {
+        mytodo.add([givecontroller.text, false]);
+      });
     }
   }
 
-  void heredeletetask(int index) {
-    print("object");
+  void heredeletetask(int index) async {
     setState(() {
       mytodo.removeAt(index);
     });
+
+
+
+    final geturl = Uri.https(
+        'todo-app-f7776-default-rtdb.firebaseio.com', "todo-data.json");
+    final getresponse = await http.get(geturl);
+
+    final Map listdata = json.decode(getresponse.body);
+    
+    final delurl = Uri.https('todo-app-f7776-default-rtdb.firebaseio.com',
+        "todo-data/${listdata.keys.elementAt(index)}.json");
+    http.delete(delurl);
   }
 
   @override
@@ -103,38 +115,59 @@ class _todoappState extends State<todoapp> {
           });
     }
 
+//displaying conditional content on main screen
+    Widget content;
+
+    if (mytodo.isEmpty) {
+      content = const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(color: Colors.black, strokeWidth: 10),
+            SizedBox(
+              height: 40,
+            ),
+            Text("start adding tasks",style: TextStyle(fontWeight: FontWeight.bold),),
+          ],
+        ),
+      );
+    } else {
+      content = ListView.builder(
+        itemCount: mytodo.length,
+        itemBuilder: (context, index) => listwidget(
+          getlist: mytodo[index],
+          deletetask: () {
+            heredeletetask(index);
+          },
+        ),
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.yellow.shade300,
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 0,
-          toolbarHeight: 105,
-          title: Text(
-            "TO DO ",
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          backgroundColor: Colors.yellow.shade300,
+          appBar: AppBar(
+            centerTitle: true,
+            elevation: 0,
+            toolbarHeight: 105,
+            title: Text(
+              "TO DO ",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Colors.yellow.shade500,
           ),
-          backgroundColor: Colors.yellow.shade500,
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
+          floatingActionButton: FloatingActionButton(
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.black,
+            onPressed: createtask,
           ),
-          backgroundColor: Colors.black,
-          onPressed: createtask,
-        ),
-        body: ListView.builder(
-          itemCount: mytodo.length,
-          itemBuilder: (context, index) => listwidget(
-            getlist: mytodo[index],
-            deletetask: () {
-              heredeletetask(index);
-            },
-          ),
-        ),
-      ),
+          body: content),
     );
   }
 }
