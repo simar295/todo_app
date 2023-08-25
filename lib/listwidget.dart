f387dd2e75a126c9cd1089932087c381458912da
Simarpreet Singh <110830402+simar295@users.noreply.github.com>
2023-08-25 21:20:56 +0530

    Update listwidget.dart

--- lib/listwidget.dart
+++ lib/listwidget.dart
@@ -5,7 +5,6 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
-
 class listwidget extends StatefulWidget {
   const listwidget(
       {super.key,
